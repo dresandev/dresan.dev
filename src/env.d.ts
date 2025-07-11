bd0827ffbf66afb21f3c38eeefca6b422a30ab86
Dresan <dresan.dev@gmail.com>
2025-07-10 23:37:09 -0500

    chore: add spotify auth env variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,13 @@
 interface ImportMetaEnv {
-  readonly PUBLIC_FORMSPREE_ID: string;
+  readonly PUBLIC_FORMSPREE_ID: string
+  readonly SPOTIFY_REFRESH_TOKEN: string
+  readonly SPOTIFY_CLIENT_ID: string
+  readonly SPOTIFY_CLIENT_SECRET: string
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv
+  readonly SPOTIFY_REFRESH_TOKEN: string
+  readonly SPOTIFY_CLIENT_ID: string
+  readonly SPOTIFY_CLIENT_SECRET: string
 }
