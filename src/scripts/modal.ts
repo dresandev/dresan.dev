e9262eac9a7dca25e3977089cf74b3f942740b3b
Dresan <dresan.dev@gmail.com>
2025-07-12 22:26:00 -0500

    auto focus first input

--- src/scripts/modal.ts
+++ src/scripts/modal.ts
@@ -4,10 +4,14 @@ const $modals = $$<HTMLDialogElement>("dialog[data-modal]")
 
 $modals.forEach((modal) => {
   modal.addEventListener("toggle", () => {
+    const firstInput = modal.querySelector("input")
+
+    firstInput?.focus()
+
     document.body.toggleAttribute("data-scroll-locked", modal.open)
   })
 
-  const $triggerButton = $$(`[data-modal-trigger-id='${modal.id}']`)!
+  const $triggerButton = $$(`[data-modal-trigger-id='${modal.id}']`)
 
   $triggerButton.forEach((button) => {
     button.addEventListener("click", () => modal.showModal())
