93a0073673bcde8f65c1016447dc075a7ad011b1
Dresan <dresan.dev@gmail.com>
2025-07-20 19:28:26 -0500

    create social media object and social media array

--- src/constants/social-media-links.ts
+++ src/constants/social-media-links.ts
@@ -3,25 +3,27 @@ import EmailIcon from "@/components/icons/email-icon.astro"
 import GitHubIcon from "@/components/icons/github-icon.astro"
 import LinkedInIcon from "@/components/icons/linked-in-icon.astro"
 
-export const SOCIAL_MEDIA_LINKS = [
-  {
+export const SOCIAL_MEDIA = {
+  github: {
     label: "GitHub",
     url: "https://github.com/dresandev",
     icon: GitHubIcon
   },
-  {
+  linkedin: {
     label: "LinkedIn",
     url: "https://www.linkedin.com/in/dresandev",
     icon: LinkedInIcon
   },
-  {
+  email: {
     label: "Email",
     url: "mailto:dresan.dev@gmail.com",
     icon: EmailIcon
   },
-  {
+  codepen: {
     label: "CodePen",
     url: "https://codepen.io/dresandev",
     icon: CodePenIcon
-  },
-]
+  }
+} as const
+
+export const SOCIAL_MEDIA_LINKS = Object.values(SOCIAL_MEDIA)
