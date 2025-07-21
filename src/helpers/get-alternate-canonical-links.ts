135439d00d51c8b640a5ab8ce5d12825f2adf5c3
Dresan <dresan.dev@gmail.com>
2025-07-21 18:58:28 -0500

    simple refactor

--- src/helpers/get-alternate-canonical-links.ts
+++ src/helpers/get-alternate-canonical-links.ts
@@ -1,15 +1,20 @@
+import { HOST_URL } from "astro:env/client"
 import { SUPPORTED_LOCALES } from "@/i18n/ui"
-import { HOST_URL } from "@/constants/urls"
 
 export const getAlternateCanonicalLinks = (canonicalUrl: string) => {
   const path = new URL(canonicalUrl).pathname
   const currentLocale = path.split("/")[1]
 
-  return SUPPORTED_LOCALES.map((locale) => {
+  const alternateCanonicals = SUPPORTED_LOCALES.map((locale) => {
     const href = canonicalUrl.replace(`/${currentLocale}`, `/${locale}`)
     return { hreflang: locale, href }
-  }).concat({
-    hreflang: "x-default",
-    href: `${HOST_URL}/`,
   })
+
+  return [
+    ...alternateCanonicals,
+    {
+      hreflang: "x-default",
+      href: `${HOST_URL}/`,
+    }
+  ]
 }
