64ae76fcb89c46ec7bd4607d2d6cdf8d9000e35f
Dresan <dresan.dev@gmail.com>
2025-08-31 18:45:29 -0500

    add x link

--- src/constants/social-media-links.ts
+++ src/constants/social-media-links.ts
@@ -2,6 +2,7 @@ import CodePenIcon from "@/components/icons/code-pen-icon.astro"
 import EmailIcon from "@/components/icons/email-icon.astro"
 import GitHubIcon from "@/components/icons/github-icon.astro"
 import LinkedInIcon from "@/components/icons/linked-in-icon.astro"
+import XIcon from "@/components/icons/x-icon.astro"
 
 export const SOCIAL_MEDIA = {
   github: {
@@ -23,6 +24,11 @@ export const SOCIAL_MEDIA = {
     label: "CodePen",
     url: "https://codepen.io/dresandev",
     icon: CodePenIcon
+  },
+  x: {
+    label: "X",
+    url: "https://x.com/dresan_dev",
+    icon: XIcon
   }
 } as const
 
